638fecb97d8a91e4fc977eb2b24922d1c6d335d8
maxxgl <maxxgl@users.noreply.github.com>
2020-04-19 22:21:18 +0000

    deploy: 46494eb5506b93f21407afe4a21049f5d34d2d38

---
+++ precache-manifest.0b5f48f52cbd55224e00cf995f383ed1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59a064503f7e828bfd4ebee9ed6979bd",
+    "url": "/watch/index.html"
+  },
+  {
+    "revision": "c069b8e8a520668e9381",
+    "url": "/watch/static/js/2.ecbc3846.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/watch/static/js/2.ecbc3846.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1389ec6bfe26ac963cae",
+    "url": "/watch/static/js/main.197e4481.chunk.js"
+  },
+  {
+    "revision": "b87c000ba6cea10627f8",
+    "url": "/watch/static/js/runtime-main.ca4d1d8a.js"
+  }
+]);
