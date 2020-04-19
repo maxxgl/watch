778dff3b64724633726a44c5e5dd0f0a91902e1b
Maxx Lehmann <mglehman@mtu.edu>
2020-04-19 14:30:18 -0500

    initialize project

---
+++ src/App/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// body {
+//   margin: 0;
+//   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+//     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+//     sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+// }
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Watch</h1>
+    </div>
+  );
+}
+
+export default App;
